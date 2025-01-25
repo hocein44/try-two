1ad2fe52b28da2283232a48b3f776430621253bc
hocein44 <hoceinsdiri@gmail.com>
2025-01-25 03:59:34 +0100

    .

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from './interfaces/product';
+import { ApiService } from './services/api.service';
+import { response } from 'express';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+products:Product[]=[];
+constructor(private apiservece:ApiService){
+}
+ngOnInit(){
+  this.apiservece.getProducts().subscribe({
+    next:(response:Product[])=>{
+      this.products=response;
+    },error:(error:Error)=>{
+      console.error('Error fetching products',error);
+    }
+  });
+ 
+}
+
 }
---
+++ frontend/src/app/interfaces/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    name: String;
+    price: Number;
+    description: String ;
+    stock: Number;
+}
+
