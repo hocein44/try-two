9f4b601f46e0706c1077060bb1badc6805792e79
hocein44 <hoceinsdiri@gmail.com>
2025-01-25 03:19:04 +0100

    .

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  data: any;
+
+  constructor(private apiService: ApiService) {}
+  ngOnInit() {
+    this.apiService.getData().subscribe(response => {
+      this.data = response;
+      console.log(this.data);
+    });
+  }
+  
 }
