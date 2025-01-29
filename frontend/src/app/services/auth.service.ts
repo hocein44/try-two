077c0c48d8a9a6cf9f2e1762b1dfcd41fb513f2a
hocein44 <hoceinsdiri@gmail.com>
2025-01-29 03:05:40 +0100

    .

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
   login(username: string, password: string): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/login`, { username, password });
   }
-
+      
   signup(username: string, password: string): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/signup`, { username, password });
   }
